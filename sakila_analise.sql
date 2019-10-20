b5c915ba2fba93424fe3d0ffde5d91f821cd7f64
Manoel Ribeiro <manoel.ribeiro@unilab.edu.br>
2019-10-20 09:08:29 -0300

    .

--- sakila_analise.sql
+++ sakila_analise.sql
@@ -3,15 +3,15 @@ use sakila;
 select 'base table', count(*) from rental
 union all
 select 'fact table1', count(*) from (
-select  r.rental_id as hash, date(rental_date) as rental_date, DATE_FORMAT(rental_date,  '%H') as rental_hour,  film_id, store_id, r.staff_id, r.customer_id, datediff(now(), rental_date) as age_rental,  datediff(now(), return_date) as age_return, sum(p.amount) as amount
+select    date(rental_date) as rental_date, DATE_FORMAT(rental_date,  '%H') as rental_hour,  film_id, store_id, r.staff_id, r.customer_id, datediff(now(), rental_date) as age_rental,  datediff(now(), return_date) as age_return, sum(p.amount) as amount
 from rental r left join inventory i
  on (r.inventory_id = i.inventory_id) 
 left join payment p on (r.rental_id = p.rental_id) 
- group by r.rental_id , date(rental_date) , DATE_FORMAT(rental_date,  '%H'),  film_id, store_id, r.staff_id, r.customer_id, date(return_date)
+ group by  date(rental_date) , DATE_FORMAT(rental_date,  '%H'),  film_id, store_id, r.staff_id, r.customer_id, date(return_date)
  ) a
 union all
 select 'fact table2', count(*) from (
-select distinct date(rental_date) as rental_date,  film_id, customer_id 
+select distinct date(rental_date) as rental_date,  film_id, customer_id, staff_id 
 from rental r left join inventory i
  on (r.inventory_id = i.inventory_id)) a
 
@@ -22,10 +22,7 @@ union all
 select 'fact table', sum(amount) from rental r left join payment p 
 on (r.rental_id = p.rental_id)
 
-
-
-
-select customer_id, sum(amount) extra_amont from payment where rental_id is null 
+select customer_id, sum(amount) extra_amount from payment where rental_id is null 
 group by customer_id
 
 
@@ -57,7 +54,7 @@ group by rental_id, customer_id
  /* film */
  select f.film_id, title, category, actor from film f 
  left join (select film_id, GROUP_CONCAT(y.name) as category from film_category fm left join category y on (fm.category_id = y.category_id) group by film_id) c on (f.film_id = c.film_id)
- left join (select film_id,  GROUP_CONCAT(concat(first_name,' ', last_name)) from film_actor fa left join actor t on (fa.actor_id = t.actor_id) group by film_id) a on  (f.film_id = a.film_id) 
+ left join (select film_id,  GROUP_CONCAT(concat(first_name,' ', last_name)) as actor from film_actor fa left join actor t on (fa.actor_id = t.actor_id) group by film_id) a on  (f.film_id = a.film_id) 
   
  /* staff */
  select staff_id, concat(first_name,' ', last_name) as name  from staff
